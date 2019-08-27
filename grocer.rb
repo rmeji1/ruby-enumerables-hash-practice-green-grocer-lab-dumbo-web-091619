9d62f50e13f44c7aee910bc07100860bf8fd0157
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 20:47:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,16 +14,7 @@ end
 def apply_coupons(cart, coupons)
   for coupon in coupons
     if cart[coupon[:item]]
-      number_of_times_to_apply = cart[coupon[:item]][:count] / coupon[:num]
-      if number_of_times_to_apply > 0 
-        cart["#{coupon[:item]} W/COUPON"] = {
-          price: coupon[:cost] / coupon[:num],
-          clearance: cart[coupon[:item]][:clearance],
-          count: number_of_times_to_apply * coupon[:num]
-        }
-        cart[coupon[:item]][:count] -= cart["#{coupon[:item]} W/COUPON"][:count]
-        p cart
-      end
+      
     end
   end
   cart
