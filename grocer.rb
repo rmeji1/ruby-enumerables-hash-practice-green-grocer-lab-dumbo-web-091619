e1afa44b523ceda46ed6aacdf48c336cf49ffed9
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 20:23:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,7 +2,7 @@ def consolidate_cart(cart)
   new_cart = cart.reduce({}) do |memo, value| 
     # value[:count] = 1
     value.each do|key, value|
-      if memo[key]
+      if !memo[key]
         memo[key][:count] += 1
       else
         value[:count] = 1
