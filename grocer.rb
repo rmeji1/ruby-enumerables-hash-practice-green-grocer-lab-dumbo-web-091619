7ff4d38b67f47fddcea039563f8dc74a451db9d3
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 20:34:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,7 +14,7 @@ end
 def apply_coupons(cart, coupons)
   for coupon in coupons
     if cart[coupon[:item]]
-      number_of_times_to_apply = cart[coupon[:item]][:count] / coupon[:num]
+      number_of_times_to_apply = 1 / coupon[:num]
       puts "We have that item #{number_of_times_to_apply}"
     end
   end
