133be6d67cebf71288a3b840dd8487f859fa4dcb
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 21:00:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,9 +37,9 @@ end
 def apply_clearance(cart)
   cart.each_value do |value|
     # apply_clearance_if_applicable(value)
-      if !!item[:clearance]
-    item[:price] -= item[:price] * 0.2
-  end
+    if !!item[:clearance]
+      item[:price] -= item[:price] * 0.2
+    end
   end
 end
 
